286c0435c413c45e5e15663d8bed5d6071941144
teddynanta <teddy.nanta99@gmail.com>
2022-10-27 12:48:52 +0700

    trying adding menus

--- resources/views/navMenu.blade.php
+++ resources/views/navMenu.blade.php
@@ -1,7 +1,36 @@
-<ul>
-  @foreach($items as $menu_item)
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="{{ $menu_item->link() }}">{{ $menu_item->title }}</a>
-  </li>
+<ul class="navbar-nav me-auto mb-2 mb-md-0">
+  @foreach ($menus as $menu)
+    @if ($menu->url == "" && $menu->parent_id === null)
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        {{ $menu->title }}
+      </a>
+      <ul class="dropdown-menu">
+        @foreach ($dropdowns as $dropdown)  
+          <li><a class="dropdown-item" href="{{ $dropdown->url }}">{{ $dropdown->title }}</a></li>
+        @endforeach
+      </ul>
+    </li>
+    @if ($menu->url !== null && $menu->)
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="{{ $menu->url }}">{{ $menu->title }}</a>
+    </li>
+    @endif
   @endforeach
 </ul>
+{{-- @if ($menu->url == "" && $menu->parent_id === null)
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    {{ $menu->title }}
+  </a>
+  <ul class="dropdown-menu">
+    @foreach ($dropdowns as $dropdown)  
+      <li><a class="dropdown-item" href="{{ $dropdown->url }}">{{ $dropdown->title }}</a></li>
+    @endforeach
+  </ul>
+</li>
+@else
+<li class="nav-item">
+  <a class="nav-link active" aria-current="page" href="{{ $menu->url }}">{{ $menu->title }}</a>
+</li>
+@endif --}}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/dump', [MenuController::class, 'getMenu']);
 
 Route::get('/admin/banner', [HomeController::class, 'banner']);
 
