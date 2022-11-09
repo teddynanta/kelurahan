199a98628aceba6f718810fa5e677c121ed25425
teddynanta <teddy.nanta99@gmail.com>
2022-11-09 14:42:54 +0700

    add

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -69,11 +69,10 @@
             @endforeach
         </div>
 
-        <div class="p-4 p-md-5 mb-4 rounded text-bg-dark">
+        <div class="p-4 p-md-5 mb-4 rounded text-bg-dark shadow-sm">
             <div class="col-md-6 px-0">
-                <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
-                <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and
-                    efficiently about what’s most interesting in this post’s contents.</p>
+                <h1 class="display-4 fst-italic">{{ $featured[0]->title }}</h1>
+                <p class="lead my-3">{{ $featured[0]->excerpt }}</p>
                 <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
             </div>
         </div>
@@ -133,59 +132,65 @@
 
         <div class="row g-5">
             <div class="col-md-8">
-                <article class="blog-post">
-                    <h2 class="blog-post-title mb-1">Another blog post</h2>
-                    <p class="blog-post-meta">December 23, 2020 by <a href="#">Jacob</a></p>
+                @foreach ($posts as $post)
+                    <article class="blog-post">
+                        <h2 class="blog-post-title mb-1">{{ $post->title }}</h2>
+                        <p class="blog-post-meta">{{ $post->created_at->format('j F, Y') }} by <a
+                                href="#">Jacob</a></p>
 
-                    <p>This is some additional paragraph placeholder content. It has been written to fill the available
-                        space and show how a longer snippet of text affects the surrounding content. We'll repeat it often
-                        to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-                    <blockquote>
-                        <p>Longer quote goes here, maybe with some <strong>emphasized text</strong> in the middle of it.</p>
-                    </blockquote>
-                    <p>This is some additional paragraph placeholder content. It has been written to fill the available
-                        space and show how a longer snippet of text affects the surrounding content. We'll repeat it often
-                        to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-                    <h3>Example table</h3>
-                    <p>And don't forget about tables in these posts:</p>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Upvotes</th>
-                                <th>Downvotes</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Alice</td>
-                                <td>10</td>
-                                <td>11</td>
-                            </tr>
-                            <tr>
-                                <td>Bob</td>
-                                <td>4</td>
-                                <td>3</td>
-                            </tr>
-                            <tr>
-                                <td>Charlie</td>
-                                <td>7</td>
-                                <td>9</td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td>Totals</td>
-                                <td>21</td>
-                                <td>23</td>
-                            </tr>
-                        </tfoot>
-                    </table>
+                        <p>This is some additional paragraph placeholder content. It has been written to fill the available
+                            space and show how a longer snippet of text affects the surrounding content. We'll repeat it
+                            often
+                            to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
+                        <blockquote>
+                            <p>Longer quote goes here, maybe with some <strong>emphasized text</strong> in the middle of it.
+                            </p>
+                        </blockquote>
+                        <p>This is some additional paragraph placeholder content. It has been written to fill the available
+                            space and show how a longer snippet of text affects the surrounding content. We'll repeat it
+                            often
+                            to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
+                        <h3>Example table</h3>
+                        <p>And don't forget about tables in these posts:</p>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Upvotes</th>
+                                    <th>Downvotes</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Alice</td>
+                                    <td>10</td>
+                                    <td>11</td>
+                                </tr>
+                                <tr>
+                                    <td>Bob</td>
+                                    <td>4</td>
+                                    <td>3</td>
+                                </tr>
+                                <tr>
+                                    <td>Charlie</td>
+                                    <td>7</td>
+                                    <td>9</td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td>Totals</td>
+                                    <td>21</td>
+                                    <td>23</td>
+                                </tr>
+                            </tfoot>
+                        </table>
 
-                    <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other
-                        highly repetitive body text used throughout.</p>
-                </article>
-
+                        <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the
+                            other
+                            highly repetitive body text used throughout.</p>
+                    </article>
+                @endforeach
                 <article class="blog-post">
                     <h2 class="blog-post-title mb-1">New feature</h2>
                     <p class="blog-post-meta">December 14, 2020 by <a href="#">Chris</a></p>
